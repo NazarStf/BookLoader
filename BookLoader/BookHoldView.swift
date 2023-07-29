65bb3e7340c11008c79e8af49db80e36afc89ebd
NazarStf <hworker98@gmail.com>
2023-07-29 19:35:44 +0300

    Create BookHoldView

---
+++ BookLoader/BookHoldView.swift
@@ -0,0 +1,35 @@
+//
+//  BookHoldView.swift
+//  BookLoader
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+struct BookHoldView: Shape {
+	
+	func path(in rect: CGRect) -> Path {
+		let cX: CGFloat = rect.midX - 28
+		let cY: CGFloat = rect.midY
+		var path = Path()
+		
+		path.move(to: CGPoint(x: cX, y: cY))
+		path.addLine(to: CGPoint(x: cX, y: cY + 12))
+		path.addLine(to: CGPoint(x: cX + 56, y: cY + 12))
+		path.addLine(to: CGPoint(x: cX + 56, y: cY))
+		return path
+	}
+}
+
+struct BookHold_Previews: PreviewProvider {
+	static var previews: some View {
+		
+		ZStack {
+			Color.black.edgesIgnoringSafeArea(.all)
+			BookHoldView()
+				.stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .miter))
+				.foregroundColor(.white)
+		}
+	}
+}
