1e0aa3873cc8bdee2fb676bd8e63f9eb49534ae7
NazarStf <hworker98@gmail.com>
2023-07-29 19:33:56 +0300

    Initial Commit

---
+++ BookLoader/BookLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLoaderApp.swift
+//  BookLoader
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
